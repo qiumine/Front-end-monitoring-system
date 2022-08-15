eb2897c05c2beb1a00eae753e45a2dba7eb4966d
qiumine <2514284562@qq.com>
2022-08-15 14:52:51 +0800

    Merge branch 'main' of https://github.com/qiumine/Front-end-monitoring-system

--- src/monitor/utils/tracker.js
+++ src/monitor/utils/tracker.js
@@ -3,7 +3,7 @@ let project = 'my-monitor';
 let logStore = 'my-monitor-store';
 let userAgent = require('user-agent');
 
-function getExtraData(){
+function getExtraData() {
     return {
         title: document.title,
         url: location.href,
@@ -11,36 +11,36 @@ function getExtraData(){
         userAgent: userAgent.parse(navigator.userAgent).name,
     }
 }
-class SendTracket{
-    constructor(){
-        this.url = `http://${project}.${host}/logstores/${logStore}/track`; //上报的路径
-        this.xhr = new XMLHttpRequest;
+class SendTracket {
+    constructor() {
+        this.url = 'http://127.0.0.1:8000/sendBeacon-server'; //上报的路径
     }
 
     send(data = {}) {
         let extraData = getExtraData();
-        let log = {...extraData,...data};
+        let log = { ...extraData, ...data };
         //对象的值不能是数字
-        for(let key in log){
-            if(typeof log[key] === 'number'){
+        for (let key in log) {
+            if (typeof log[key] === 'number') {
                 log[key] = `${log[key]}`;
             }
         }
         console.log('log:', log);
-        let body = JSON.stringify({
-            __logs__: [log]
-        });
-        this.xhr.open('POST', this.url, true);
-        this.xhr.setRequestHeader('Content-Type', 'application/json'); //请求体类型
-        this.xhr.setRequestHeader('x-log-apiversion', '0.6.0'); //本版号
-        this.xhr.setRequestHeader('x-log-bodyrawsize', body.length); //请求体大小
-        this.xhr.onload = function(){
-           // console.log(this.xhr.response);
-        }
-        this.xhr.onerror = function(error){
-           // console.log(error);
-        }
-        this.xhr.send(body);
+        // let body = JSON.stringify({
+        //     __logs__: [log]
+        // });
+        // this.xhr.open('POST', this.url, true);
+        // this.xhr.setRequestHeader('Content-Type', 'application/json'); //请求体类型
+        // this.xhr.setRequestHeader('x-log-apiversion', '0.6.0'); //本版号
+        // this.xhr.setRequestHeader('x-log-bodyrawsize', body.length); //请求体大小
+        // this.xhr.onload = function(){
+        //    // console.log(this.xhr.response);
+        // }
+        // this.xhr.onerror = function(error){
+        //    // console.log(error);
+        // }
+        // this.xhr.send(body);
+        navigator.sendBeacon(this.url, JSON.stringify(log));
     }
 }
 
