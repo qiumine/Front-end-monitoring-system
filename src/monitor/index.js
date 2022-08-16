eb7fb409225b95789d7170b0e9996e877eff9a16
qiumine <2514284562@qq.com>
2022-08-17 00:44:05 +0800

    add pv引用

--- src/monitor/index.js
+++ src/monitor/index.js
@@ -1,10 +1,12 @@
 import { blankScreen } from './lib/blankScreen';
-import {injectJsError} from './lib/jsError';
-import {injectXHR} from './lib/xhr';
-import {timing} from './lib/timing';
+import { injectJsError } from './lib/jsError';
+import { injectXHR } from './lib/xhr';
+import { timing } from './lib/timing';
+import { pv } from './lib/pv';
 
 
 injectJsError();
 injectXHR();
 blankScreen();
 timing();
+pv();
