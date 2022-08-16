9a6598b36702dfb50b163b9248221931fe149ba3
wxg-github <106816611+wxg-github@users.noreply.github.com>
2022-08-16 08:53:39 +0800

    Delete backend directory

--- backend/camera/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from django.urls import re_path as url
-from . import views
-urlpatterns = [
-    path("",views.getdata,name="getdata"),
-]
--- backend/web/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.decorators import csrf
-from django.http import HttpResponse
-from . import models
-# Create your views here.
-def savedata(request):
-    return HttpResponse("OK")
