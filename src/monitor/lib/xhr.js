462e330d7d4ed175ff9bcde33a91bc1f9f2774ee
qiumine <2514284562@qq.com>
2022-08-17 15:33:25 +0800

    修改 xhr

--- src/monitor/lib/xhr.js
+++ src/monitor/lib/xhr.js
@@ -1,33 +1,32 @@
 import tracker from "../utils/tracker";
-import { injectJsError } from "./jsError";
 
 export function injectXHR() {
     let XMLHttpRequest = window.XMLHttpRequest;
     let oldOpen = XMLHttpRequest.prototype.open;
     XMLHttpRequest.prototype.open = function (method, url, async) {
-        if(!url.match(/logstores/) && !url.match(/sockjs/)){
-            this.logData = {method, url, async};
+        if (!url.match(/logstores/) && !url.match(/sockjs/)) {
+            this.logData = { method, url, async };
         }
         //this.logData = {method, url, async};
         return oldOpen.apply(this, arguments);
     }
     let oldSend = XMLHttpRequest.prototype.send;
     XMLHttpRequest.prototype.send = function (body) {
-        if(this.logData){
+        if (this.logData) {
             let startTime = Date.now(); //记录发送前时间
             let handler = (type) => (event) => {
                 let duration = Date.now() - startTime;
                 let status = this.status; //200 500
                 let statusText = this.statusText; //OK Server Error
                 tracker.send({
-                    kind : 'stability',
-                    type : 'xhr',
-                    eventType : type, // load error abort
-                    pathname : this.logData.url, //请求路径
-                    status : status + '-' + statusText, //他状态码
+                    kind: 'stability',
+                    type: 'xhr',
+                    eventType: type, // load error abort
+                    pathname: this.logData.url, //请求路径
+                    status: status + '-' + statusText, //他状态码
                     duration, // 持续时间
-                    response : this.response? JSON.stringify(this.response) : '', //响应体
-                    params : body || '' 
+                    response: this.response ? JSON.stringify(this.response) : '', //响应体
+                    params: body || ''
                 });
             }
             this.addEventListener('load', handler('load'), false);
